66b48f7f22e98c0f6f42d9ceb5fb05b5660c17cc
Тетяна Деревянко <t.derevyanko1209@gmail.com>
2024-03-12 16:31:46 +0200

    Add project files.

---
+++ WebAPIToDoList/TodoContext.cs
@@ -0,0 +1,22 @@
+﻿namespace WebAPIToDoList;
+using Microsoft.EntityFrameworkCore;
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+        //Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+    public TodoContext()
+    {
+        //Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("TodoList");
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+}
